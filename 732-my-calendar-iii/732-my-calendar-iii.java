ff2ab93ab5c732a4356c1444747f45fffecf2a01
Rahul Gorai <97347964+rahul003t@users.noreply.github.com>
2022-10-07 08:22:16 +0530

    Time: 123 ms (73.22%), Space: 43 MB (94.10%) - LeetHub

---
+++ 732-my-calendar-iii/732-my-calendar-iii.java
@@ -0,0 +1,22 @@
+class MyCalendarThree {
+    private TreeMap<Integer, Integer> timeline = new TreeMap<>();
+    public int book(int s, int e) {
+        
+        timeline.put(s, timeline.getOrDefault(s, 0) + 1); // 1 new event will be starting at [s]
+        timeline.put(e, timeline.getOrDefault(e, 0) - 1); // 1 new event will be ending at [e];
+        
+        int ongoing = 0, k = 0;
+        
+        for (int v : timeline.values())
+            k = Math.max(k, ongoing += v);
+        
+        
+        return k;
+    }
+}
+
+/**
+ * Your MyCalendarThree object will be instantiated and called as such:
+ * MyCalendarThree obj = new MyCalendarThree();
+ * int param_1 = obj.book(start,end);
+ */
