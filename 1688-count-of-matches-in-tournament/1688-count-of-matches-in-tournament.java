be847aa9aa6fb69778c12b6f12831f4ec8dc92ad
Rahul Gorai <97347964+rahul003t@users.noreply.github.com>
2022-10-11 11:02:40 +0530

    Time: 0 ms (100.00%), Space: 41 MB (47.49%) - LeetHub

---
+++ 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int numberOfMatches(int n) {
+        
+        // int sum=2;
+        // while(n != 1)
+        // {
+        //     n = n/2;
+        //     sum += n;
+        // }
+        // return sum;
+        
+        return n-1;
+    }
+}
