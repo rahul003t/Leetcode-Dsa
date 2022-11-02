87ced02b9f2357744916f2f825e9c8360feb3eb4
Rahul Gorai <97347964+rahul003t@users.noreply.github.com>
2022-11-02 19:09:41 +0530

    Time: 2 ms (67.66%), Space: 39.5 MB (95.12%) - LeetHub

---
+++ 0377-combination-sum-iv/0377-combination-sum-iv.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int combinationSum4(int[] nums, int target) {
+        
+        Arrays.sort(nums);
+        int[] res = new int[target+1];
+        
+        for(int i=0;i<res.length;i++)
+        {
+            for(int num: nums)
+            {
+                if(num > i)
+                {
+                    break;
+                }
+                else if( num == i )
+                {
+                    res[i] +=1;
+                }
+                else
+                {
+                    res[i] += res[i-num];
+                }
+            }
+        }
+        
+        return res[target];
+    }
+}
