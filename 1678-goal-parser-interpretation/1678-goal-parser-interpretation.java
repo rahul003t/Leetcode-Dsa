97ee9a13f3b24cf14f0e11d2335aeb6467a29b29
Rahul Gorai <97347964+rahul003t@users.noreply.github.com>
2022-11-15 00:29:12 +0530

    Time: 1 ms (93.67%), Space: 42.1 MB (47.18%) - LeetHub

--- 1678-goal-parser-interpretation/1678-goal-parser-interpretation.java
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.java
@@ -1,6 +1,28 @@
 class Solution {
     public String interpret(String command) {
         
-        return command.replace("()","o").replace("(al)","al");
+        //return command.replace("()","o").replace("(al)","al");
+        
+        StringBuilder sb = new StringBuilder();
+        
+        for(int i=0;i<command.length();i++)
+        {
+            if(command.charAt(i) =='(' && command.charAt(i+1) == ')')
+            {
+                sb.append("o");
+                i+=1;
+            }
+            else if(command.charAt(i) =='(' && command.charAt(i+1) != ')')
+            {
+                sb.append("al");
+                i+=3;
+            }
+            else
+            {
+                sb.append(command.charAt(i));
+            }
+        }
+        
+        return sb.toString();
     }
 }
