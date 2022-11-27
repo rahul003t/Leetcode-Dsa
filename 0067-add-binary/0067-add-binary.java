eb792b100c7dde2de969ccf02cb768afed0a7039
Rahul Gorai <97347964+rahul003t@users.noreply.github.com>
2022-11-27 17:04:25 +0530

    Time: 14 ms (12.16%), Space: 43.8 MB (13.47%) - LeetHub

---
+++ 0067-add-binary/0067-add-binary.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String addBinary(String a, String b) {
+        int carry=0;
+        String result = "";
+        int alen = a.length();
+        int blen = b.length();
+        int i=0;
+        while( i<alen || i<blen|| carry!=0){
+            int x =0;
+            if(i<alen && a.charAt(alen-1-i) =='1') x =1;
+            int y=0;
+            if(i<blen && b.charAt(blen-1-i) =='1')  y =1;
+            
+            result = (x+y+carry)%2 +result;
+            carry = (x+y+carry)/2;
+            i++;
+            
+            
+            
+        }
+        return result;
+        
+    }
+}
