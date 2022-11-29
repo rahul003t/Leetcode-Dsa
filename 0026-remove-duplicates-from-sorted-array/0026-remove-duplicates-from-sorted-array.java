9e5c9cef6df7edc4299740e251d43660cbf1ad87
Rahul Gorai <97347964+rahul003t@users.noreply.github.com>
2022-11-29 23:13:16 +0530

    Time: 1 ms (100.00%), Space: 44.1 MB (86.88%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -1,20 +1,31 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        int[] large = new int[nums.length];
-        int largest = Integer.MIN_VALUE;
-        int jdx =0;
-        for(int idx =0; idx <nums.length;idx++){
-            if(nums[idx] > largest){
-                largest = nums[idx];
-                large[jdx] = largest;
-                jdx++;
+//         int[] large = new int[nums.length];
+//         int largest = Integer.MIN_VALUE;
+//         int jdx =0;
+//         for(int idx =0; idx <nums.length;idx++){
+//             if(nums[idx] > largest){
+//                 largest = nums[idx];
+//                 large[jdx] = largest;
+//                 jdx++;
+//             }
+//         }
+        
+//         for(int idx =0;idx<large.length;idx++){
+//             nums[idx] = large[idx];
+//         }
+        
+//         return jdx;
+        
+        int j=0;
+        for(int i=0;i<nums.length;i++)
+        {
+            if(nums[j] != nums[i])
+            {
+                nums[++j] = nums[i];
             }
         }
         
-        for(int idx =0;idx<large.length;idx++){
-            nums[idx] = large[idx];
-        }
-        
-        return jdx;
+        return ++j;
     }
 }
