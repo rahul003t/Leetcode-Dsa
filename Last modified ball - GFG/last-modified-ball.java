2e5bcc824e913924d2b9d9b1c58b0a0118fee3d3
Rahul Gorai <97347964+rahul003t@users.noreply.github.com>
2023-01-09 15:12:01 +0530

    Added solution - LeetHub

--- Last modified ball - GFG/last-modified-ball.java
+++ Last modified ball - GFG/last-modified-ball.java
@@ -54,18 +54,13 @@ class GFG {
 class Solution {
     int solve(int N, int[] A) {
         // code here
-        
-        //traversing  the array from the right side as the question given with decrementing
-        for(int i=N-1; i>=0; i--)
+        for(int i=N-1;i>=0;i--)
         {
-            //if arrayindex is less than 1  return the the value
-            if(A[i] < 9)
+            if (A[i] < 9 )
             {
-                //as given array is 1 base indexing rteunn index+1
                 return i+1;
             }
         }
-        //if all array contains >=9 return 0
         return 0;
     }
 };
