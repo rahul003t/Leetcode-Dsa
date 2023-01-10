f05d5d9bdf8f2c9b94d3979d3deda40a29f4a39b
Rahul Gorai <97347964+rahul003t@users.noreply.github.com>
2023-01-10 18:37:54 +0530

    Added solution - LeetHub

--- Transform to Sum Tree - GFG/transform-to-sum-tree.java
+++ Transform to Sum Tree - GFG/transform-to-sum-tree.java
@@ -133,13 +133,17 @@ class Solution{
     
     public static int addRoots(Node root)
     {
+        //if there is no roots return 0
         if(root == null)
         {
             return 0;
         }
+        //storing roots data
         int x = root.data;
+        //calculating roots (left+right) data
         root.data = addRoots(root.left)+addRoots(root.right);
         
+        //return own roots data + (left+right) data
         return root.data+x;
     }
 }
