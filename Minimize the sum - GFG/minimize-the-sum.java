f67bd3e8d7771330e4fff267f6a163a76213a9ea
Rahul Gorai <97347964+rahul003t@users.noreply.github.com>
2023-01-12 18:25:48 +0530

    Added solution - LeetHub

--- Minimize the sum - GFG/minimize-the-sum.java
+++ Minimize the sum - GFG/minimize-the-sum.java
@@ -12,18 +12,21 @@ import java.io.*;
 class Solution {
     long minimizeSum(int N, int arr[]) {
         // code here
-        
+        // as question mentioned minimize the sum ,I am using PriorityQueue 
+        // as it use meanHeap in very efficient way.
         long ans =0;
         PriorityQueue<Integer> pq = new PriorityQueue<>();
-        
+        //itterate the array and all elemnets in the queue
         for(int i: arr)
         {
             pq.add(i);
         }
-        
+        // while there is more than a single element in the list continue the list
         while(pq.size() >= 2)
         {
+            //add the curr two element
             int curr = pq.remove()+ pq.remove();
+            //adding the element to the answer
             ans += curr;
             pq.add(curr);
         }
