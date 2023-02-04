a8a912a10f137fe34a43af284396b116c51c52ce
Rahul Gorai <97347964+rahul003t@users.noreply.github.com>
2023-02-04 23:25:33 +0530

    Added solution - LeetHub

--- Max Sum without Adjacents - GFG/max-sum-without-adjacents.java
+++ Max Sum without Adjacents - GFG/max-sum-without-adjacents.java
@@ -31,35 +31,34 @@ public class Main {
 
 
 //User function Template for Java
-class Solution {
+
+ class Solution {
 
     int findMaxSum(int arr[], int n) {
 
-        int dp[] = new int[n+1];
+        // code here
 
-        Arrays.fill(dp,-1);
+        if(n==1)
 
-        return recur(0,arr,dp);
+            return arr[0];         // if n==1 then there is nothing to compare
 
-    }
+        if(n>2)                    // the last third element can be added to last element only 
 
-    int recur(int i, int arr[],int dp[]){
-
-        // base case
-
-        if(i>=arr.length) return 0;
-
-        if(dp[i]!=-1) return dp[i];
-
-    
-
-        int pick = arr[i]+recur(i+2,arr,dp);
-
-        int notpick = recur(i+1,arr,dp);
+            arr[n-3]+=arr[n-1];   // jumping three index from this point will cost it ArrayIndexOutOfBound
 
         
 
-        return dp[i]=Math.max(pick,notpick);
+        for(int i=n-4;i>=0;i--)
+
+        {
+
+            arr[i]+=Math.max(arr[i+2],arr[i+3]); //Checking at each step for the more optimized jump
+
+        }                                        //and storing it on the current array
+
+        
+
+        return Math.max(arr[0],arr[1]);    //The answer can be available on any of the index 0th or 1st so return the maximum one.
 
     }
 
