e11d2c60c08e55659bd6845cbc14b17aaffb6778
Rahul Gorai <97347964+rahul003t@users.noreply.github.com>
2023-02-12 19:05:06 +0530

    Added solution - LeetHub

---
+++ Prime List - GFG/prime-list.java
@@ -0,0 +1,100 @@
+//{ Driver Code Starts
+//Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+class Node{
+    Node next;
+    int val;
+    public Node(int data){
+        val=data;
+        next=null;
+    }
+}
+
+class Main
+{
+    public static void main(String args[])throws Exception
+    {
+        BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out=new PrintWriter(System.out);
+        
+        int t;
+        t=Integer.parseInt(in.readLine());
+        while(t-->0){
+            int n;
+            n=Integer.parseInt(in.readLine());
+            Node head,tail;
+            String s[]=in.readLine().trim().split(" ");
+            int num=Integer.parseInt(s[0]);
+            head=new Node(num);
+            tail=head;
+            for(int i=1;i<n;i++){
+                num=Integer.parseInt(s[i]);
+                tail.next=new Node(num);
+                tail=tail.next;
+            }
+            Solution ob=new Solution();
+            Node temp=ob.primeList(head);
+            while(temp!=null){
+                out.print(temp.val+" ");
+                temp=temp.next;
+            }
+            out.println();
+        }
+        out.close();
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+/*
+class Node{
+    Node next;
+    int val;
+    public Node(int data){
+        val=data;
+        next=null;
+    }
+}
+*/
+class Solution
+{
+   Node primeList(Node head){
+        //code here
+        Node temp = head;
+        while(temp != null){
+            temp.val = nearestPrime(temp.val);
+            temp = temp.next;
+        }
+        return head;
+    }
+
+    public static int nearestPrime(int n){
+        int i = n;
+        int j = n;
+
+        while(true){
+            if(isPrime(i)){
+                return i;
+            }else if(isPrime(j)){
+                return j;
+            }
+            i--;j++;
+        }
+    }
+
+    public static boolean isPrime(int n) {
+        if (n <= 1) {
+            return false;
+        }
+        int sqrt = (int) Math.sqrt(n);
+        for (int i = 2; i <= sqrt; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
