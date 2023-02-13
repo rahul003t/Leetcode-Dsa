59b3d1f2713d23e0494a8a2e12d32f8824969dcf
Rahul Gorai <97347964+rahul003t@users.noreply.github.com>
2023-02-13 11:55:18 +0530

    Time: 1 ms (76.64%), Space: 42.5 MB (57.92%) - LeetHub

---
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
@@ -0,0 +1,183 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        
+        List<List<Integer>> result = new ArrayList<>();
+        
+        if(root == null)
+        {
+            return result;
+        }
+
+        
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        
+        while(!queue.isEmpty())
+        {
+            int size = queue.size();
+            List<Integer> currLevel = new ArrayList<>();
+            for(int i=0;i<size;i++ )
+            {
+                TreeNode current = queue.remove();
+                currLevel.add(current.val);
+                
+                if(current.left != null)
+                {
+                    queue.add(current.left);
+                }
+                
+                if(current.right != null)
+                {
+                    queue.add(current.right);
+                }
+            }
+            result.add(currLevel);
+        }
+        
+        return result;
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+      //         List<List<Integer>> result = new ArrayList<>();
+//         if(root == null){
+//             return result;
+//         }
+//         Queue<TreeNode> queue = new LinkedList<>();
+//         queue.add(root);
+//         while(!queue.isEmpty()){
+//             int size = queue.size();
+//             List<Integer> currentLevel = new ArrayList<>();
+//             for(int i=0;i<size;i++){
+//                 TreeNode current = queue.remove();
+//                 currentLevel.add(current.val);
+//                 if(current.left != null){
+//                     queue.add(current.left);
+//                 }
+//                 if(current.right != null){
+//                     queue.add(current.right);
+//                 }
+                
+//             }
+            
+//             result.add(currentLevel);
+//         }
+//         return result;  
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+}
