40e3b6137ae8764208f30306e3ea0186dad6ca26
Rahul Gorai <97347964+rahul003t@users.noreply.github.com>
2023-02-19 20:00:09 +0530

    Time: 0 ms (100.00%), Space: 42.6 MB (15.28%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
@@ -15,29 +15,35 @@
  */
 class Solution {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-        List<List<Integer>> res = new ArrayList<>();
-        if(root == null) {
-            return res;
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        List<List<Integer>> res = new LinkedList<>();
+        if(root == null) return res;
+        queue.add(root);
+        boolean flag=false;
+        while(queue.size()>0)
+        {
+            int size = queue.size();
+            LinkedList<Integer> subList = new LinkedList<>();
+            for(int i =0; i<size;i++)
+            {
+                TreeNode node = queue.poll();
+                if(node.left !=null) queue.add(node.left);
+                if(node.right != null) queue.add(node.right);
+                if(flag) 
+                {
+                    subList.addFirst(node.val);//addFirst method adds the elements at the beginning of the list
+                }
+                else {
+                    subList.addLast(node.val);//the addLast method adds the elements at the end
+                }
+                
+            }  
+            flag=!flag;
+          res.add(subList);
+            
         }
-        Queue<TreeNode> q = new LinkedList<>();
-
-        q.add(root);
-        int level = 0;
-        while(!q.isEmpty()) {
-            int size = q.size();
-            List<Integer> temp = new ArrayList<>();
-            for(int i=0; i < size; i++) {
-                TreeNode curr = q.poll();
-                temp.add(curr.val);
-
-                if(curr.left != null) q.add(curr.left);
-                if(curr.right != null) q.add(curr.right);
-            }
-            if(level % 2 == 1) Collections.reverse(temp);
-            res.add(new ArrayList<Integer>(temp));
-            level ++;
-        }
-
         return res;
+
+
     }
 }
