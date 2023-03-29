49eb9f1d985e2a7b232cae6a644e633d2e9b94df
Rahul Gorai <97347964+rahul003t@users.noreply.github.com>
2023-03-29 23:39:52 +0530

    Added solution - LeetHub

---
+++ Count the Substrings - GFG/count-the-substrings.java
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+//Initial Template for Java
+import java.io.*;
+import java.util.*; 
+class GFG{
+    public static void main(String args[]) throws IOException { 
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        
+        while(t-- > 0){
+            String S = read.readLine().trim();
+            Solution ob = new Solution();
+            int ans = ob.countSubstring(S);
+            System.out.println(ans);
+        }
+    } 
+} 
+// } Driver Code Ends
+
+
+//User function Template for Java
+class Solution 
+{ 
+   int countSubstring(String S) 
+    { 
+        // code here
+        HashMap<Integer,Integer> map = new HashMap<>();
+        map.put(0,1);
+        int sum=0,ans=0;
+        for(int i=0;i<S.length();i++) {
+            if(S.charAt(i)>='A' && S.charAt(i)<='Z') {
+                sum++;
+            } else {
+                sum--;
+            }
+            
+            if(map.containsKey(sum)) {
+                ans+=map.get(sum);
+                map.put(sum,map.get(sum)+1);
+            } else {
+                map.put(sum,1);
+            }
+        }
+        return ans;
+    }
+} 
