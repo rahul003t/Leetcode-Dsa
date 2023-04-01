42c9e7d413ff25fe3dacbc9b693631085778b23f
Rahul Gorai <97347964+rahul003t@users.noreply.github.com>
2023-04-01 11:36:10 +0530

    Added solution - LeetHub

--- Make Array Elements Equal - GFG/make-array-elements-equal.java
+++ Make Array Elements Equal - GFG/make-array-elements-equal.java
@@ -32,8 +32,8 @@ class Solution {
     public long minOperations(int N) {
         // Code here
         
-        int mid =N/2;
         
-        return N%2 == 0? (long)mid * (long)mid : (long)(mid) * (long)(mid+1);
+        int mid = N/2;
+        return N%2 ==0?(long)mid*(long)mid:(long)mid*(long)(mid+1);
     }
 }
