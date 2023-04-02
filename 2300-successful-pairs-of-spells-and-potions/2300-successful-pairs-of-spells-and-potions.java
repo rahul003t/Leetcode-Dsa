c1e8127bfb6baf511a71b5567aa8320afb412bc4
Rahul Gorai <97347964+rahul003t@users.noreply.github.com>
2023-04-02 10:10:15 +0530

    Time: 72 ms (80.58%), Space: 62.6 MB (35.12%) - LeetHub

--- 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.java
+++ 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.java
@@ -1,12 +1,12 @@
 class Solution {
     public int[] successfulPairs(int[] spells, int[] potions, long success) {
-        int low=0;
-        int r=potions.length-1;
+        // int low=0;
+        // int r=potions.length-1;
         int ar[]=new int[spells.length];
         Arrays.sort(potions);
         for(int i=0;i<spells.length;i++){
-            low=0;
-            r=potions.length-1;
+            int low=0;
+            int r=potions.length-1;
             while(low<=r){
                 int mid=low+(r-low)/2;
                 long prodc=(long)spells[i]*potions[mid];
