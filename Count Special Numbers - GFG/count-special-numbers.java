eac778c5c44a0abba1b386dc0d7435ad2515d633
Rahul Gorai <97347964+rahul003t@users.noreply.github.com>
2023-04-05 21:03:10 +0530

    Added solution - LeetHub

---
+++ Count Special Numbers - GFG/count-special-numbers.java
@@ -0,0 +1,88 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+    static class FastReader{ 
+        BufferedReader br; 
+        StringTokenizer st; 
+  
+        public FastReader(){ 
+            br = new BufferedReader(new InputStreamReader(System.in)); 
+        } 
+  
+        String next(){ 
+            while (st == null || !st.hasMoreElements()){ 
+                try{ st = new StringTokenizer(br.readLine()); } catch (IOException  e){ e.printStackTrace(); } 
+            } 
+            return st.nextToken(); 
+        } 
+  
+        String nextLine(){ 
+            String str = ""; 
+            try{ str = br.readLine(); } catch (IOException e) { e.printStackTrace(); } 
+            return str; 
+        } 
+
+        Integer nextInt(){
+            return Integer.parseInt(next());
+        }
+
+        Long nextLong(){
+            return Long.parseLong(next());
+        }
+    }
+    
+    public static void main(String[] args) throws IOException {
+        FastReader sc = new FastReader();
+        int t = sc.nextInt(); // Inputting the testcases
+        PrintWriter out = new PrintWriter(System.out);
+        while (t-- > 0) {
+
+            int i = 0;
+            int N = sc.nextInt();
+
+            int arr[] = new int[(int)(N)];
+
+            for (i = 0; i < N; i++) {
+                arr[i] = sc.nextInt();
+            }
+
+            Solution ob = new Solution();
+            out.println(ob.countSpecialNumbers(N, arr));
+        }
+        out.flush();
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    public int countSpecialNumbers(int N, int arr[]) {
+        int max = Arrays.stream(arr).max().getAsInt();
+        int[] hash = new int[max+1];
+        Arrays.fill(hash,0);
+        for(int i=0;i<N;i++){
+            int start = arr[i];
+            if(hash[start]<=1){
+                for(int j=start;j<=max;j=j+start){
+                    hash[j]++;
+                }
+            }
+        }
+        int count = 0;
+        for(int j=0;j<N;j++){
+            if(hash[arr[j]]>1){
+                count++;
+            }
+        }
+        
+    
+        return count;
+    }
+}
+
