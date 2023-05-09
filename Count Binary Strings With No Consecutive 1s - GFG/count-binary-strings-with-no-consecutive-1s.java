c99de4772d8e2baba18700360dda120b1cacca26
Rahul Gorai <97347964+rahul003t@users.noreply.github.com>
2023-05-09 21:03:37 +0530

    Added solution - LeetHub

---
+++ Count Binary Strings With No Consecutive 1s - GFG/count-binary-strings-with-no-consecutive-1s.java
@@ -0,0 +1,67 @@
+//{ Driver Code Starts
+// Initial Template for Java
+// Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t =
+            Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while (t-- > 0) {
+
+            long N = Long.parseLong(br.readLine().trim());
+
+            Solution ob = new Solution();
+            System.out.println(ob.countStrings(N));
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+class Solution {
+    final int MOD = 1000000007;
+    long[][] multiply(long[][] a, long[][] b) {
+        long[][] matrix = new long[2][2];
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                long count = 0;
+                for (int k = 0; k < 2; k++) {
+                    count = (count % MOD + (a[i][k] * b[k][j]) % MOD) % MOD;
+                }
+                matrix[i][j] = count % MOD;
+            }
+        }
+        return matrix;
+    }
+
+    long[][] matrixExponentiation(long[][] v, long n) {
+        if (n == 1) {
+            return v;
+        }
+        long[][] temp = matrixExponentiation(v, n / 2);
+        long[][] ans = multiply(temp, temp);
+        if (n % 2 == 1) {
+            ans = multiply(ans, v);
+        }
+        return ans;
+    }
+    public int countStrings(long N) {
+        // Code here
+           long[][] matrix = new long[2][2];
+        matrix[0][0] = 1;
+        matrix[0][1] = 1;
+        matrix[1][0] = 1;
+        matrix[1][1] = 0;
+        matrix = matrixExponentiation(matrix, N);
+        return (int) ((matrix[0][0] + matrix[0][1]) % MOD);
+    }
+}
+
