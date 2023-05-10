eae23bc8d4c73d8a4f03190c3e073f895bdfb74e
Rahul Gorai <97347964+rahul003t@users.noreply.github.com>
2023-05-10 22:27:51 +0530

    Added solution - LeetHub

--- Total Cuts - GFG/total-cuts.java
+++ Total Cuts - GFG/total-cuts.java
@@ -61,35 +61,28 @@ class GFG {
 class Solution {
     public static int totalCuts(int N, int K, int[] A) {
         // code here
-        int max=Integer.MIN_VALUE;
-        int min=Integer.MAX_VALUE;
-        int[] B = new int[N];
+        int imax= A[0];
+        int jmin= A[N-1];
+        int count=0;
         
-        for(int i = 0;i<A.length;i++){
-            B[i]=A[i];
+        int[] maxArr = new int[N];
+        maxArr[0]=A[0];
+        int[] minArr = new int[N];
+        minArr[N-1]= A[N-1];
+        
+        for(int i=1;i<N;i++){
+            if(A[i]>imax) imax= A[i];
+            if(A[N-1-i]< jmin) jmin=A[N-1-i];
+            
+            maxArr[i]=imax;
+            minArr[N-1-i] =jmin;
         }
         
-        for(int i = 0;i<A.length;i++){
-            if(A[i] > max){
-                max=A[i];
-            }
-            else A[i]=max;
+        for(int i=0;i<N-1;i++){
+            if(maxArr[i]+minArr[i+1]>=K) count++;
         }
-        
-        
-        for(int i = N-1;i>=0;i--){
-            if(B[i] < min){
-                min=B[i];
-            }else B[i]=min;
-        }
-        
-        
-       int count = 0;
-       
-       for(int i = 0;i<N-1;i++){
-           if(A[i]+B[i+1]>=K) count++;
-       }
         
         return count;
     }
 }
+
