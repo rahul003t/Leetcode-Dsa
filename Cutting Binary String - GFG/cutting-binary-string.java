9640f2c6301690765b9dd6f54e7c48700d3a8710
Rahul Gorai <97347964+rahul003t@users.noreply.github.com>
2023-05-16 19:15:48 +0530

    Added solution - LeetHub

--- Cutting Binary String - GFG/cutting-binary-string.java
+++ Cutting Binary String - GFG/cutting-binary-string.java
@@ -24,7 +24,7 @@ class GFG{
 //User function Template for Java
 
 class Solution{
-    static int cuts(String str) {
+   static int cuts(String str) {
 
 		for (int i = 22; i >= 0; i--)
 			str = str.replaceAll(Long.toBinaryString((long) Math.pow(5, i)), "#");
