834da864b2b8fbe30811c4ede4d6b1bb1b074484
Rahul Gorai <97347964+rahul003t@users.noreply.github.com>
2023-06-21 21:24:43 +0530

    Added solution - LeetHub

---
+++ Reverse Coding - GFG/reverse-coding.java
@@ -0,0 +1,34 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            String S[] = read.readLine().split(" ");
+            
+            int n = Integer.parseInt(S[0]);
+
+            Solution ob = new Solution();
+            System.out.println(ob.sumOfNaturals(n));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+class Solution {
+    static int sumOfNaturals(int n) {
+        // code here
+        
+         int m=(int)Math.pow(10,9)+7;
+        long p=n;
+        return(int)((p*(p+1)/2)%m);
+    }
+};
