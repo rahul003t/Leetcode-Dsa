173874a2d8666c9cf51b4d4ac9f5cefee7e8e04a
Rahul Gorai <97347964+rahul003t@users.noreply.github.com>
2023-06-28 22:42:52 +0530

    Added solution - LeetHub

--- Maximum Depth Of Binary Tree - GFG/maximum-depth-of-binary-tree.java
+++ Maximum Depth Of Binary Tree - GFG/maximum-depth-of-binary-tree.java
@@ -113,9 +113,9 @@ class Solution {
     if(root == null)
     {
         return 0;
-    }   
+    }
     
-    return Math.max(maxDepth(root.left),maxDepth(root.right))+1;
+    return Math.max(maxDepth(root.right),maxDepth(root.left))+1;
 
   }
 }
