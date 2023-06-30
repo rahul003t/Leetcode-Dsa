ed16810059c53e3106f14ec099297974f5aa2cc0
Rahul Gorai <97347964+rahul003t@users.noreply.github.com>
2023-06-30 20:53:07 +0530

    Added solution - LeetHub

--- Is Binary Number Multiple of 3 - GFG/is-binary-number-multiple-of-3.java
+++ Is Binary Number Multiple of 3 - GFG/is-binary-number-multiple-of-3.java
@@ -31,16 +31,17 @@ class Solution {
         
         for(int i=0;i<s.length();i++)
         {
-            if(i%2 ==0)
+            if(i%2 == 0)
             {
-                even += (s.charAt(i)-'0');
-            }else
+                even += (s.charAt(i)- '0');
+            }
+            
+            else
             {
                 odd += (s.charAt(i)-'0');
             }
-            
         }
         
-        return (even-odd)%3==0?1:0;
+        return (even-odd)%3 ==0 ?1:0;
     }
 }
