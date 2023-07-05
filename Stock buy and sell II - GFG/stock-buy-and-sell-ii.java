90c3a5339b511095df34d32a521c8acdb12f9299
Rahul Gorai <97347964+rahul003t@users.noreply.github.com>
2023-07-05 23:34:25 +0530

    Added solution - LeetHub

--- Stock buy and sell II - GFG/stock-buy-and-sell-ii.java
+++ Stock buy and sell II - GFG/stock-buy-and-sell-ii.java
@@ -56,14 +56,16 @@ class GFG {
 
 
 
+
 class Solution {
     public static int stockBuyAndSell(int n, int[] prices) {
         // code here
         int profit=0;
-        //int n = prices.length;
-        for(int i=1;i<n;i++){
-            if(prices[i]>prices[i-1]){
-                profit+=(prices[i]-prices[i-1]);
+        for(int i=1;i<n;i++)
+        {
+            if(prices[i] > prices[i-1])
+            {
+                profit += (prices[i]-prices[i-1]);
             }
         }
         return profit;
