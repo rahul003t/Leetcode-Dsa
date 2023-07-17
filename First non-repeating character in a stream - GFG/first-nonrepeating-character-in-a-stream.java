305c4a3bdbaf60ad25ffe119eb0f6fd197ff1d88
Rahul Gorai <97347964+rahul003t@users.noreply.github.com>
2023-07-17 12:23:45 +0530

    Added solution - LeetHub

---
+++ First non-repeating character in a stream - GFG/first-nonrepeating-character-in-a-stream.java
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out=new PrintWriter(System.out);
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            String A = br.readLine().trim();
+            Solution ob = new Solution();
+            String ans = ob.FirstNonRepeating(A);
+            out.println(ans);
+        }
+        out.close();
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution
+{
+     public String FirstNonRepeating(String A)
+    {
+       int n = A.length();
+       Queue<Character> q = new LinkedList<>();
+       int[] map = new int[128];
+       StringBuilder ans =new StringBuilder();
+       
+       for(char c : A.toCharArray()){ 
+           map[c]++;
+           
+           if(map[c]==1) q.add(c);
+           
+           while(!q.isEmpty() && map[q.peek()]>1) q.remove();
+           ans.append(q.isEmpty() ? '#' : q.peek());
+       }
+       return ans.toString();
+    }
+}
